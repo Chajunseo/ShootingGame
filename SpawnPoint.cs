8cf69037a78f1fd71d0d5aa3da61024ee54a16a7
Chajunseo <sem03020@naver.com>
2016-10-20 17:42:24 +0900

    Add files via upload

---
+++ SpawnPoint.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPoint : MonoBehaviour {
+	public GameObject debri;
+	public float interval = 1f;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("SpawnDebris");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	IEnumerator SpawnDebris(){
+		while (true) {
+			Instantiate (debri, transform.position, Quaternion.identity);
+			yield return new WaitForSeconds(interval);
+		}
+	}
+}
+
